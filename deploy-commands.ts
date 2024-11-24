47de7a11a5ccacd848004fd97c90fc70e57dc7a1
NamTheDev <thica.doi@gmail.com>
2024-11-24 08:28:54 +0700

    refactor: Simplify deploy-commands.ts and remove README.md

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -3,13 +3,11 @@ import { readdirSync } from 'fs';
 import config from './config';
 const { DISCORD_APPLICATION_ID: clientId, DISCORD_BOT_TOKEN: token } = config;
 
-// Validate required environment variables
 if (!clientId || !token) {
     console.error('Error: Missing DISCORD_APPLICATION_ID or DISCORD_TOKEN in environment variables.');
     process.exit(1);
 }
 
-// Define the structure for commands
 interface Command {
     data: {
         toJSON: () => object;
@@ -17,12 +15,10 @@ interface Command {
     execute: Function;
 }
 
-// Initialize the commands array
 const commands: object[] = [];
 const commandsPath = 'src/commands';
 const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
 
-// Load all command files
 for (const file of commandFiles) {
     const filePath = `./${commandsPath}/${file}`;
     const command: Command = require(filePath).default;
@@ -34,17 +30,13 @@ for (const file of commandFiles) {
     }
 }
 
-// Initialize REST client
 const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
     try {
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        // Clear existing commands
         await rest.put(Routes.applicationCommands(clientId), { body: [] });
-
-        // Register new commands
         await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
         console.log(`Successfully reloaded application (/) commands.`);
